88f234c8b10d0826292e93a24e3db7e0f9f1090e
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-11-14 09:51:46 -0500

    map now works im just gonna make it appear now

--- mapPakage.js
+++
@@ -1,8 +0,0 @@
-var mapPackage = {name: "The Ultimate Map",
-                  size: {width: 75, height: 50},
-                  tile: Cell,
-                  fill: "#",
-                  room: {max:{width: 15, height: 15},
-                         min:{width: 5, height: 5}},
-                  roomDensity: .2,
-                  hallDensity: 1.4}
