7a1a3d3fc2a9307f69d1701f4ab7c91ac5cecf84
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-11-14 10:32:30 -0500

    ok

--- map.js
+++ map.js
@@ -1,58 +1,30 @@
-class map{
-  constructor(data){
-    this._width = this._datatype(data.size.width, "int");
-    this._height = this._datatype(date.size.height, "int");
-    this._tile = this._datatype(date.tile, "obj");
-    this._fill = this._dataCheck(data.fill, "str");
+class Map {
+  constructor(){
+    this.width = this._width;
+    this.height = this._height;
+    this.map = this._map;
   }
 
-  _dataCheck(item, type){
-     try{ var retVal = Utils.typeCheck(item, type, "Map constructor"); }
-     catch(e){
-       e += "The Map class constructor expects an object of the format: ";
-       e += "{name: str, size: {width: int, height: int}, tile: Class, fill:str}. ";
-       e += "One or more part of this object failed to pass type checking or was ";
-       e += "not present in the starting data object.";
-       throw new Error(e);
-     }
-     return retVal;
-   }
-
-  _initMap(size, fill){
-  var retAry = [];
-  var cells = {x:undefined,y:undefined};
-  for (var i = 0; i < size.height; i++) {
-    for (var j = 0; j < size.width; j++) {
-      cells = {x:i,y:j};
-      retAry.push(cells);
+  _map(width=10, height=10){
+    var retAry = [];
+    var obj = {x:undefined,y:undefined, image:""};
+    for (var i = 0; i < width; i++) {
+      for (var j = 0; j < height; j++) {
+        obj = {x:j, y:i}
+        retAry.push(obj);
       }
     }
     return retAry;
   }
 
-  update(dungeon){
-    for (var y = 0; y < this.cells.length; y++) {
-      for (var x = 0; x < this.cells[y].length; x++) {
-        this.cells[y][x].update(dungeon, {x: x, y: y});
-      }
+  _mapAppear(width, length){
+    var bob = this._map;
+    if (bob.x <= 0 || bob.y >= width) {
+      bob.image = "=";
     }
-    for (var y = 0; y < this.cells.length; y++) {
-      for (var x = 0; x < this.cells[y].length; x++) {
-        this.cells[y][x].reset();
-      }
+    if (bob.x >= length || bob.y <= 0) {
+      bob.image = "|";
     }
+    return bob
   }
-
-  set width(int){ this._width = Utils.typeCheck(int, "int", "Map.width"); }
-  get width(){ return this._width; }
-
-  set height(int){ this._heigth = Utils.typeCheck(int, "int", "Map.height"); }
-  get height(){ return this._height; }
-
-  set tile(cls){ this._tile = Utils.typeCheck(cls, "class", "Map.width"); }
-  get tile(){ return this._tile; }
-
-  set fill(str){ this._fill = Utils.typeCheck(str, "str", "Map.width"); }
-  get fill(){ return this._fill; }
-
 }
