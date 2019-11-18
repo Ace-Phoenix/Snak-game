5de9e6a8582613790bcde1e08d024a323d27b45b
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-11-18 09:47:50 -0500

    pushing map that works

--- map.js
+++ map.js
@@ -3,37 +3,48 @@ class Map {
     this.width = this._width;
     this.height = this._height;
     this.map = this._map;
-    this.image = "";
   }
 
   _map(width=10, height=10){
     var retAry = [];
     var obj = {x:undefined,y:undefined, image:""};
-    for (var i = 0; i < width; i++) {
-      for (var j = 0; j < height; j++) {
+    for (var i = 0; i < height; i++) {
+      for (var j = 0; j < width; j++) {
         obj = {x:j, y:i}
         retAry.push(obj);
       }
     }
     return retAry;
   }
+//corners start at top left then top right then bottom left then bottom right
+//corner 1 = {x:0, y:0}
+//corner 2 = {x:max-1, y:0}
+//corner 3 = {x:0, y:max-1}
+//corner 4 = {x:max-1, y:max-1}
 
-
-  _mapAppear(width=10, length=10){
-    var bob = this._map;
-    var length = bob.length;
-    for (var i = 0; i < bob.x.length; i++) {
-      if (bob.x <= 0 || bob.y >= width) {
-        bob.image = "=";
+  _mapAppear(width=10, height=10){
+    var bob = this._map(width, height);
+    var whole = width*height;
+    for (var i = 0; i < whole; i++) {
+      if (bob[i].y <= 0 || bob[i].x >= width-1) {
+        bob[i].image = "=";
+      }else {
+        bob[i].image = "#"
       }
     }
-    for (var i = 0; i < bob.y.length; i++) {
-      if (bob.x >= length || bob.y <= 0) {
-        bob.image = "|";
+    for (var j = 0; j < whole; j++) {
+      if (bob[j].y >= height-1 || bob[j].x <=0) {
+        bob[j].image = "|";//going to need to put a <br> here
+      }
+      if (bob[j].y == 0 || bob[j].y == height-1) {
+        if (bob[j].x == 0 || bob[j].x == width-1) {
+          bob[j].image = "+";
+        }
       }
     }
     return bob;
   }
+
 }
-var joe = document.getElementsByClassName("snek-map")
-joe.innerHTML = Map._mapAppear;
+var joe = new Map
+document.getElementById("map").innerHTML = joe._mapAppear(10, 10)
