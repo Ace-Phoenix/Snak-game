59a5d3489e4e0cb1820817dfcf3370d20705d54f
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-11-19 10:04:46 -0500

    i hate life and imma end it all

--- cell.js
+++ cell.js
@@ -1,8 +1,19 @@
 class Cell {
-  constructor() {
+  constructor(name, type = "map") {
+    this.name = Utils.typeCheck(name, "str", "cell constructor name")
+    this.type = Utils.typeCheck(type, "str", "cell constructor type");
   }
-  toString(){
 
+  get name(){ return this._name; }
+  set name(str){ this._name = Utils.typeCheck(str, "str", "Cell.name");}
+
+  get type(){return this._type}
+  set type(type){
+    this._type = Utils.typeCheck(type, "str", "Cell.name");
+    this._snek = Utils.typeCheck(type, "str", "Cell.name");
   }
 
+    toString(){
+      return this._name;
+  }
 }
--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 class Map {
-  constructor(){
-    this.width = this._width;
-    this.height = this._height;
-    this.map = this._map;
+  constructor() {
+   this._name = this._dataCheck(data.name, "str");
+   this._width = this._dataCheck(data.size.width, "int");
+   this._height = this._dataCheck(data.size.height, "int");
+
+   his._cells = this._map(this.width, this._height)
   }
 
   _map(width, height){
@@ -14,37 +16,6 @@ class Map {
         retAry.push(obj);
       }
     }
-    console.log(retAry);
     return retAry;
   }
-//corners start at top left then top right then bottom left then bottom right
-//corner 1 = {x:0, y:0}
-//corner 2 = {x:max-1, y:0}
-//corner 3 = {x:0, y:max-1}
-//corner 4 = {x:max-1, y:max-1}
-
-  _mapAppear(width=10, height=10){
-    var bob = this._map(width, height);
-    var whole = width*height;
-    for (var i = 0; i < whole; i++) {
-      if (bob[i].y <= 0 || bob[i].x >= width-1) {
-        bob[i] = "=";
-      }
-    }
-    for (var j = 0; j < whole; j++) {
-      if (bob[j].y >= height-1 || bob[j].x <=0) {
-        bob[j] = "|<br>";//going to need to put a <br> here
-      }
-      if (bob[j].y == 0 || bob[j].y == height-1) {
-        if (bob[j].x == 0 || bob[j].x == width-1) {
-          bob[j] = "+<br>";
-        }
-      }else {
-        bob[i] = "#"
-      }
-    }
-    return bob.toString();
-  }
 }
-lol = new Map();
-document.getElementById("map").innerHTML = lol._mapAppear(10,10);
