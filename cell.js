794f63cadcf92433cec3cc7cfde7db62aa4a8cd2
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-11-20 10:50:40 -0500

    ok dont question it its a merge

--- cell.js
+++ cell.js
@@ -1,10 +1,10 @@
 class Cell {
  constructor(type = "map") {
-    this._obj = {x:undefined,y:undefined,name:"#"}
+    this._obj = {x:undefined,y:undefined,name:"#"};
     this._type = Utils.typeCheck(type, "str", "cell constructor type");
   }
 
-  get obj(){return this._obj}
+  get obj(){return this._obj;}
 
 
   get type(){return this._type;}
--- map.js
+++ map.js
@@ -2,15 +2,13 @@ class Map {
   constructor(data) {
    this._width = undefined;
    this._height = undefined;
-   this._cells = this._map(this._width, this._height);
   }
 
   _map(width, height){
     var retAry = [];
     for (var i = 0; i < height; i++) {
       for (var j = 0; j < width; j++) {
-        obj = {name:"o",x:j, y:i};
-        let obj = new Cell();
+         let obj = new Cell();
         obj.obj.x = i;
         obj.obj.y = j;
         retAry.push(obj);
@@ -20,37 +18,14 @@ class Map {
   }
 
   _mapAppear(width=10, height=10){
-        var f = new Cell(name);
     var bob = this._map(width, height);
     var whole = width*height;
     for (var i = 0; i < whole; i++) {
-      if (bob[i].y <= 0 || bob[i].x >= width-1) {
-        f.name = "=";
+      if (bob[i].obj.y <= 0 || bob[i].obj.x >= width-1) {
+        bob[i].name = "=";
       }
     }
-
     for (var j = 0; j < whole; j++) {
-      if (bob[j].y >= height-1 || bob[j].x <=0) {
-        bob[j] = "|"+"<br>";//going to need to put a <br> here
-      }
-      if (bob[j].y == 0 || bob[j].y == height-1) {
-        if (bob[j].x == 0 || bob[j].x == width-1) {
-          bob[j] = "+" +"<br>";
-        f.name = "|";
-      }
-      }
-      if (bob[j].y == 0 || bob[j].y == height-1) {
-        if (bob[j].x == 0 || bob[j].x == width-1) {
-          f.name = "+";
-        }
-      }
-      }
-  
-  return bob;
-}
-}
-var joe = new Map();
-document.getElementById("map").innerHTML = joe._mapAppear(10,10);
       if (bob[i].obj.y <= 0 || bob[i].obj.x >= width-1) {
         bob[i].name = "=";
       }
@@ -69,4 +44,4 @@ document.getElementById("map").innerHTML = joe._mapAppear(10,10);
 }
 
 var joe = new Map();
-document.getElementById("map").innerHTML = joe._mapAppear()
+document.getElementById("map").innerHTML = joe._mapAppear();
