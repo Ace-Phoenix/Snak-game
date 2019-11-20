e7731ec38b7d2c81d963b37006c916af6cd8d1d9
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2019-11-20 10:27:45 -0500

    oooooooook

--- cell.js
+++ cell.js
@@ -1,19 +1,19 @@
 class Cell {
   constructor(name, type = "map") {
-    this._name = Utils.typeCheck(name, "str", "cell constructor name")
+    this._name = Utils.typeCheck(name, "str", "cell constructor name");
     this._type = Utils.typeCheck(type, "str", "cell constructor type");
   }
 
   get name(){ return this._name; }
   set name(str){ this._name = Utils.typeCheck(str, "str", "Cell.name");}
 
-  get type(){return this._type}
+  get type(){return this._type;}
   set type(type){
     this._type = Utils.typeCheck(type, "str", "Cell.name");
     this._snek = Utils.typeCheck(type, "str", "Cell.name");
   }
 
     toString(){
-      return this.name;
+      return this._name;
   }
 }
--- map.js
+++ map.js
@@ -3,16 +3,16 @@ class Map {
    this._name = "";
    this._width = undefined;
    this._height = undefined;
-
    this._cells = this._map(this._width, this._height);
   }
 
   _map(width, height){
+    var f = new Cell(name);
     var retAry = [];
     var obj = {x:undefined,y:undefined};
     for (var i = 0; i < height; i++) {
       for (var j = 0; j < width; j++) {
-        obj = {x:j, y:i};
+        obj = {name:"o",x:j, y:i};
         retAry.push(obj);
       }
     }
@@ -20,11 +20,12 @@ class Map {
   }
 
   _mapAppear(width=10, height=10){
+        var f = new Cell(name);
     var bob = this._map(width, height);
     var whole = width*height;
     for (var i = 0; i < whole; i++) {
       if (bob[i].y <= 0 || bob[i].x >= width-1) {
-        this._cells._name = "=";
+        f.name = "=";
       }
     }
 
@@ -35,19 +36,17 @@ class Map {
       if (bob[j].y == 0 || bob[j].y == height-1) {
         if (bob[j].x == 0 || bob[j].x == width-1) {
           bob[j] = "+" +"<br>";
-        this._cells._name = "|";
+        f.name = "|";
+      }
       }
       if (bob[j].y == 0 || bob[j].y == height-1) {
         if (bob[j].x == 0 || bob[j].x == width-1) {
-          this._cells._name = "+";
+          f.name = "+";
         }
-      }else {
-        this._cells._name = "#";
       }
-    }
+      }
+  
   return bob;
-  }
-
 }
 }
 var joe = new Map();
