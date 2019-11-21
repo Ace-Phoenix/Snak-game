6728eabfc8d0968e6b4c4f0d36f6784b977b22e2
Kade <kadethebomb@gmail.com>
2019-11-21 09:04:25 -0500

    more addition to food for pull

--- food.js
+++ food.js
@@ -2,4 +2,13 @@ class Food {
   constructor{
     this._type = type;//different types of food?
   }
+  //getters
+  get type(){return this._type;}
+
+  //setters
+  set type(type){this.type = type;}
+
+  function makeFood(){
+    
+  }
 }
