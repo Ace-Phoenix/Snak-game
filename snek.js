b980e5f49f6fb4ad0df8261e0448e60a9532b8e3
Pizaman15 <47151158+Pizaman15@users.noreply.github.com>
2019-12-09 18:15:24 -0500

    Merge branch 'master' of https://github.com/DeadlyNinja1201/Snak-game

--- snek.js
+++ snek.js
@@ -1,11 +1,11 @@
 //checks for wasd to push
-class Snek {
+class Snek extends Map {
   constructor(){
     this._head = "O";//will have its own grapic mabey a "D"
     this._body = "o";//length
     this._direction = this._direction;//way the snek is moving which is either North, South, East, West
-    this._pos = {x:77,y:77};//will be wherer the snek is on the map
-}
+    this._pos = {x:undefined,y:undefined};//will be wherer the snek is on the map
+  }
   //getters
   get head(){return this._head;}
   get body(){return this._body;}
@@ -15,17 +15,98 @@ class Snek {
   //setters
   set body(length){this._body = length;}
   set direction(direction){this.direction = direction;}
-  set pos(pos){this._pos = pos;}
-  _overlap(){
+  set posX(posX){this._posX = posX;}
+  set posY(posY){this._posY = posY;}
 
+  directionChange(keyPressed){
+    //North
+    if(keyPressed == "w"){
+      return this.constentsMove("North");
+    }
+    //South
+    if(keyPressed == "s"){
+      return this.constentsMove("South");
+    }
+    //East
+    if(keyPressed == "d"){
+      return this.constentsMove("East");
+    }
+    //West
+    if(keyPressed == "a"){
+      return this.constentsMove("West");
+    }
   }
-}
-//what is jakes gitname
+
+
+  _constentsMove(directional){
+      if(directional == "North" && this._posY < map._height){
+        this._pos.y += 1;
+      }
+      if(directional == "South" && this._pos.y){
+        this._pos.y -=1;
+      }
+      if(directional == "East" && this._pos.x){
+        this._pos.x +=1;
+      }
+      if(directional == "West" && this._pos.x){
+        this._pos.x -=1;
+      }
+      console.log(this._pos.x);
+      console.log(this._pos.y);
+      }, 1000);
+
+    _overlap(){
+      var direction = this.direction;
+      var nextMove = this.pos;
+      if (direction == "North"){
+        nextMove.y ++;
+      }
+      else if(direction == "South"){
+        nextMove.y --;
+      }
+      else if(direction == "East"){
+        nextMove.x ++;
+      }
+      else if(direction == "West"){
+        nextMove.x --;
+      }
+      var bodyLoc = Map._snekLoc();
+      if (nextMove == bodyLoc){
+        //end game....... Kinda really just makes snek invisible
+        this._head = " ";
+        this._body = " ";
+      }
+      if(nextMove.x == 0){
+        this._head = " ";
+        this._body = " ";
+      }
+      if(nextMove.y == 0){
+        this._head = " ";
+        this._body = " ";
+      }
+      if(nextMove.x == Map.width){
+        this._head = " ";
+        this._body = " ";
+      }
+      if(nextMove.y == Map.length){
+        this._head = " ";
+        this._body = " ";
+      }
+      if(nextMove == { Map.width,Map.length}){
+        this._head = " ";
+        this._body = " ";
+      }
+
+    }
+  }
+
+
+
 //checks for wasd to pushed
 //
-
-      var sneks = new Snek();
-  document.addEventListener("keyup", function work(event){
+var sneekysneekysnek = this.pos;
+document.addEventListener("keyup",
+function work(event) {
   var push = undefined;
   if (event.key == "w" || event.key == "W") {
     push = "w";
@@ -39,53 +120,7 @@ class Snek {
   if (event.key == "d" || event.key == "D") {
     push = "d";
   }
-  //in the five fucking minets he had my computer
-  return inter(push);
+
+  var sneks = new Snek();
+  return sneks.directionChange(push);
 });
-function inter(rep){
-  var gameplay=0
-  while(gameplay<=100){
-  if(rep == "w"){
-      constentsMove(rep);
-      gameplay++
-  }
-   else if(rep == "s"){
-      constentsMove(rep);
-      gameplay++
-  }
-  else if(rep == "d"){
-      constentsMove(rep);
-      gameplay++
-  }
-  else if(rep == "a"){
-      constentsMove(rep);
-      gameplay++
-  }
-  else{
-    return "not ready";
-  }
-}
-}
- function constentsMove(directional){
-    setTimeout(function(){
-        if(directional == "w"){
-        sneks.pos.y += 1;
-            console.log(sneks.pos);
-
-        }
-        if(directional == "s"){
-        sneks.pos.y -=1;
-            console.log(sneks.pos);
-
-        }
-        if(directional == "d"){
-        sneks.pos.x +=1;
-            console.log(sneks.pos);
-
-        }
-        if(directional == "a"){
-          sneks.pos.x -=1;
-            console.log(sneks.pos);
-        }
-    },5000);
-  }
